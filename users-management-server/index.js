7083138854b9c8c058eddd1e9a8991ed66c98fb5
sucharita4867 <sucharitasardar7@gmail.com>
2025-10-28 11:45:05 +0530

    added a new project in user management in server side

--- users-management-server/index.js
+++ users-management-server/index.js
@@ -2,8 +2,18 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (res, req) => {
+app.get("/", (req, res) => {
   res.send("user server is available");
+});
+const users = [
+  { id: 1, name: "farish", email: "farish@gmail.com" },
+  { id: 2, name: "racho", email: "racho@gmail.com" },
+  { id: 3, name: "sucho", email: "sucho@gmail.com" },
+  { id: 4, name: "anu", email: "anu@gmail.com" },
+];
+
+app.get("/users", (req, res) => {
+  res.send(users);
 });
 
 app.listen(port, () => {
