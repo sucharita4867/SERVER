ba72225ac328b445df07e889fb78ea374c5cccc5
sucharita4867 <sucharitasardar7@gmail.com>
2025-10-28 17:25:52 +0530

    added and show data in ui

--- users-management-server/index.js
+++ users-management-server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("user server is available");
@@ -14,6 +18,14 @@ const users = [
 
 app.get("/users", (req, res) => {
   res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log("post method called", res.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
 });
 
 app.listen(port, () => {
