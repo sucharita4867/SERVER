6d4510923e3d5aaa72825800a5ca56b50daa323c
sucharita4867 <sucharitasardar7@gmail.com>
2025-10-28 16:20:44 +0530

    added create api show in ui

--- users-management-client/src/App.jsx
+++ users-management-client/src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import Users from "./Components/Users";
+
+const usersPromise = fetch("http://localhost:3000/users").then((res) =>
+  res.json()
+);
 
 function App() {
   return (
     <>
       <h1>User Management</h1>
+      <Users usersPromise={usersPromise}></Users>
     </>
   );
 }
---
+++ users-management-client/src/Components/Users.jsx
@@ -0,0 +1,9 @@
+import React, { use } from "react";
+
+const Users = ({ usersPromise }) => {
+  const users = use(usersPromise);
+  console.log(users);
+  return <div></div>;
+};
+
+export default Users;
