f88dcef79902f0d8a4e8bda81e55c88e7b54a7d8
sucharita4867 <sucharitasardar7@gmail.com>
2025-10-29 20:26:07 +0530

    added price and descriptions

--- src/Components/SinglePhone.jsx
+++ src/Components/SinglePhone.jsx
@@ -9,9 +9,10 @@ const SinglePhone = () => {
       <h2>{phone.name} </h2>
       <div className="border border-red-800">
         <figure>
-
-        <img src={phone.image} alt="" />
+          <img src={phone.image} alt="" />
         </figure>
+        <p>Price : {phone.price}</p>
+        <p>Description : {phone.description}</p>
       </div>
     </div>
   );
