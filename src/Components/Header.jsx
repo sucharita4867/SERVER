681cde9fa6593280b85646fd2130f9ad0c21a7b0
sucharita4867 <sucharitasardar7@gmail.com>
2025-10-29 20:11:33 +0530

    added css classes

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <Link to="/phones">Phones</Link>
+      tis is header
+    </div>
+  );
+};
+
+export default Header;
